adb33be8e0637583b15d1802b1c010e5dbcd5690
Faukkirik <kote_33419@mail.ru>
2023-10-16 15:47:28 +0300

    2 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Todolists} from "./components/Todolists";
+import {TaskPropsType, Todolists} from "./components/Todolists";
 
+export type FilteredPropsTaskType = 'all' | 'active' | 'completed'
 function App() {
-    const task1 = [
+    const [task, setTask]=useState<TaskPropsType[]>([
         {id: 1, title: 'HTML/CSS', isDone: true},
         {id: 2, title: 'JS/TS', isDone: true},
-        {id: 3, title: 'React', isDone: false}
-    ]
-    const task2 = [
-        {id: 1, title: 'Milk', isDone: false},
-        {id: 2, title: 'Beer', isDone: true},
-        {id: 3, title: 'Cola', isDone: false}
-    ]
+        {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'Redux', isDone: false}
+    ])
+    const [filter, setFilter] = useState<FilteredPropsTaskType>('all')
+    let taskForTodolist = task
+    if (filter === 'active'){
+        taskForTodolist = task.filter(el => el.isDone)
+    }
+    if (filter === 'completed'){
+        taskForTodolist = task.filter(el => !el.isDone)
+    }
+    const removeTask = (id: number) => {
+        setTask(taskForTodolist.filter((el)=> el.id !== id))
+    }
+    const filteredTasks = (value: FilteredPropsTaskType) => {
+        setFilter(value)
+    }
+
     return (
         <div className="App">
-            <Todolists title={'What to lear'} task={task1}/>
-            <Todolists title={'What to buy'} task={task2}/>
+            <Todolists
+                title={'What to lear'}
+                task={taskForTodolist}
+                removeTask={removeTask}
+                filteredTasks={filteredTasks}
+            />
         </div>
     );
 }
--- src/components/Todolists.tsx
+++ src/components/Todolists.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {FilteredPropsTaskType} from "../App";
 
 export type TaskPropsType = {
     id: number
@@ -8,6 +9,8 @@ export type TaskPropsType = {
 type TodolistPropsType = {
     title: string
     task: TaskPropsType[]
+    removeTask: (id: number) => void
+    filteredTasks: (value: FilteredPropsTaskType) => void
 }
 export const Todolists = ( props: TodolistPropsType ) => {
     return (
@@ -18,14 +21,19 @@ export const Todolists = ( props: TodolistPropsType ) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={props.task[0].isDone}/> <span>{props.task[0].title}</span></li>
-                <li><input type="checkbox" checked={props.task[1].isDone}/> <span>{props.task[1].title}</span></li>
-                <li><input type="checkbox" checked={props.task[2].isDone}/> <span>{props.task[2].title}</span></li>
+                {props.task.map((el) => {
+                    return (
+                        <li>
+                            <input type="checkbox" checked={el.isDone}/> <span>{el.title}</span>
+                            <button onClick={ ()=>{props.removeTask(el.id)} }>x</button>
+                        </li>
+                    )
+                })}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={ ()=>{props.filteredTasks('all')} }>All</button>
+                <button onClick={ ()=>{props.filteredTasks('active')}}>Active</button>
+                <button onClick={ ()=>{props.filteredTasks('completed')}}>Completed</button>
             </div>
         </div>
     );
